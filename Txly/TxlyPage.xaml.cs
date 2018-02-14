c265bc7647f863d59379fd2df347666f64ddc9d7
kenleegit <ahkenlee@yahoo.com.hk>
2018-02-14 21:44:01 +0800

    Added webview showing Cbox

--- Txly/TxlyPage.xaml.cs
+++ Txly/TxlyPage.xaml.cs
@@ -17,6 +17,13 @@ namespace Txly
             viewModel.MediaPlayer.Play();
             label1.Text = viewModel.MediaPlayer.ToString();
             label2.Text = viewModel.MediaPlayer.MediaExtractor.ToString();
+
+            var url = new UrlWebViewSource
+            {
+                Url = "https://www4.cbox.ws/box/?boxid=4327572&boxtag=Cf5HyA&tid=1&tkey=ce16b6434b5e5b3b"
+            };
+            webView.Source = url;
+
             //These flows errors on Android.
             //label3.Text = viewModel.MediaPlayer.Duration.ToString();
             //label4.Text = viewModel.MediaPlayer.Position.ToString();
